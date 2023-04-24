9bd91de633d2f0ccdfc3cf7c997574fae4a5fb45
Owolabi Pius <opius2007@gmail.com>
2023-04-24 09:51:19 +0100

    Sorting algorithm updates

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,50 +1,42 @@
-#include "sort.h"
 /**
- * counting_sort - Sorts array of integers with counting sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
+ * counting_sort - Sorts an array of integers using counting sort algorithm
+ * @array: Array of integers to be sorted
+ * @size: Size of the array to be sorted
+ * Return: void
  */
 void counting_sort(int *array, size_t size)
 {
-	int max_num = 0;
-	size_t i, j;
+	unsigned int i, k, count;
+	int *copy, *count_arr;
 
-	int *count_array = NULL, *sorted_array = NULL;
-
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
+	k = array[0];
+	for (i = 0; i < size; i++)
+		k = (array[i] > k) ? array[i] : k;
+	count_arr = malloc(sizeof(int) * (k + 1));
+	if (count_arr == NULL)
+		return;
+	for (i = 0; i <= k; i++)
+		count_arr[i] = 0;
+	for (i = 0; i < size; i++)
+		count_arr[array[i]] += 1;
+	for (i = 1; i <= k; i++)
+		count_arr[i] += count_arr[i - 1];
+	print_array(count_arr, k + 1);
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+	{
+		free(count_arr);
+		return;
+	}
 	for (i = 0; i < size; i++)
 	{
-		if (array[i] > max_num)
-			max_num = array[i];
-		}
-	count_array = malloc(sizeof(int) * (max_num + 1));
-	if (!count_array)
-		return;
-	sorted_array = malloc(sizeof(int) * size);
-	if (!sorted_array)
-	{
-		free(count_array);
-		return;
-		}
-	for (i = 0; i <= max_num; i++)
-		count_array[i] = 0;
+		copy[count_arr[array[i]] - 1] = array[i];
+		count_arr[array[i]] -= 1;
+	}
 	for (i = 0; i < size; i++)
-		count_array[array[i]]++;
-	printf("Count array:\n");
-	for (i = 0; i <= max_num; i++)
-		printf("%d: %d\n", (int)i, count_array[i]);
-	printf("\n");
-	for (i = 1; i <= max_num; i++)
-		count_array[i] += count_array[i - 1];
-	for (i = size - 1; (int)i >= 0; i--)
-	{
-		j = count_array[array[i]] - 1;
-		sorted_array[j] = array[i];
-		count_array[array[i]]--;
-		}
-	for (i = 0; i < size; i++)
-		array[i] = sorted_array[i];
-	free(count_array);
-	free(sorted_array);
+		array[i] = copy[i];
+	free(copy);
+	free(count_arr);
 }
